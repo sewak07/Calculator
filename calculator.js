edeac2110dade66024303a0ddb4dc0256ba0bd85
sewak07 <160036481+sewak07@users.noreply.github.com>
2025-01-29 21:27:26 +0545

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,25 @@
+let display=document.querySelector("#display"); //select the display
+
+let btns=document.querySelectorAll(".btnNum"); //select the buttons with class .butNum
+for(let btn of btns){                       //loop to get all the buttons
+    btn.addEventListener("click",()=>{      // event listner
+         display.value+=btn.textContent;    //display the text written in the button to display
+            if(btn.textContent=="c"){
+                display.value="";            // clears the display
+            }
+        }
+)}
+
+let Equal=document.querySelector(".btnEqual");// select the button with class .btnEqual
+Equal.addEventListener("click",()=>{         //event listner
+   try{
+    display.value=eval(display.value);//evaluate the expression
+   }catch(error){
+    display.value="error"; //handle invalid expression
+   }
+   
+})
+
+
+
+
